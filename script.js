35d12689ddf40513b47487d652e9d8ba0c71894b
alexandre <alexandre.queudeville@ynov.com>
2025-03-05 14:07:29 +0100

    search bar

--- script.js
+++ script.js
@@ -26,3 +26,19 @@ function getUsers() {
         })
         .catch(error => console.error('Erreur:', error));
 }
+
+const cityInput = document.getElementById('city');
+const weatherData = document.getElementById('weatherData');
+
+cityInput.addEventListener('input', function() {
+  const city = cityInput.value.trim();
+  if (city !== '') {
+    fetch(`/weather/${city}`)
+      .then(response => response.json())
+      .then(data => {
+        weatherData.innerText = JSON.stringify(data, null, 2);
+      });
+  } else {
+    weatherData.innerText = '';
+  }
+});
